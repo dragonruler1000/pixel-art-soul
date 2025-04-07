a176afd182f9f3e4ff64de09d6b94b26436172bc
dragonruler1000 <dev@me.minecraftchest2.us>
2025-04-06 21:28:50 -0500

    Removed orchestrator, added an asset pack, and added the enemy and main scenes.

---
+++ enemy/enemy.gd
@@ -0,0 +1,36 @@
+extends RigidBody2D
+
+@onready var nav_agent: NavigationAgent2D = $NavigationAgent2D
+var velocity = Vector2.ZERO
+var player: Node2D = null
+var path_update_timer := 0.0
+var path_update_interval := 0.5 # Update path every half second
+
+func _ready():
+	# Optional tuning
+	nav_agent.path_desired_distance = 8.0
+	nav_agent.target_desired_distance = 8.0
+
+
+func set_target_node(target: Node2D):
+	player = target
+
+func _physics_process(delta):
+	# Update path every few frames
+	if player != null:
+		path_update_timer -= delta
+		if path_update_timer <= 0.0:
+			nav_agent.target_position = player.global_position
+			path_update_timer = path_update_interval
+
+	if nav_agent.is_navigation_finished():
+		linear_velocity = Vector2.ZERO
+		return
+
+	var next_path_point = nav_agent.get_next_path_position()
+	var direction = (next_path_point - global_position).normalized()
+	var speed = 100.0
+	linear_velocity = direction * speed
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
---
+++ main_area/main.gd
@@ -0,0 +1,41 @@
+extends Node
+
+@export var enemy_scene: PackedScene
+var score
+
+func _ready():
+	new_game()
+
+
+func game_over() -> void:
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+func new_game():
+	score=0
+	$Player.start($StartPosition.position)
+	$StartTimer.start() # Replace with function body.
+
+
+func _on_mob_timer_timeout() -> void:
+	var enemy = enemy_scene.instantiate()
+
+	# Spawn position
+	var spawn_marker = $EnemySpawn
+	enemy.global_position = spawn_marker.global_position
+	
+	add_child(enemy)
+
+	# Set player as the target for pathfinding
+	var player = $Player
+	enemy.set_target_node($Player)
+
+	
+
+
+func _on_score_timer_timeout():
+	score += 1
+
+
+func _on_start_timer_timeout() -> void:
+	$MobTimer.start()
+	$ScoreTimer.start()
