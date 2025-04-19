76633424c8b639f79e7c550220e2bc63f0ed5bc8
dragonruler1000 <dev@me.minecraftchest2.us>
2025-04-18 21:25:38 -0500

    I dont remember

--- misc/ui.gd
+++ misc/ui.gd
@@ -1,8 +1,7 @@
 extends Control
 @onready var progress_bar: ProgressBar = $PlayerHealth
 var max_hp = PlayerVariables.max_health
-var current_hp = PlayerVariables.current_health
 func _ready():
 	progress_bar.min_value = 0
 	progress_bar.max_value = max_hp
-	progress_bar.value = current_hp
+	progress_bar.value = PlayerVariables.current_health
